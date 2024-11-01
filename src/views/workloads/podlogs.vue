d232d60a6b3bc79dccc5f27581bbcacb2546cf57
xiwolaisi <347047131@qq.com>
2024-11-01 15:03:03 +0800

    j监听日志变化，修改日志为流式

--- src/views/workloads/podlogs.vue
+++ src/views/workloads/podlogs.vue
@@ -12,49 +12,65 @@
 
 </template>
 <script>
-import { getPodContainers,getPodLogs } from "@/api/pods"
+import { getPodContainers } from "@/api/pods";
 
 export default {
-  data(){
+  data() {
     return {
-      containers:{},
-      selectContainer:"",
-      logs:""
-    }
+      containers: {},
+      selectContainer: "",
+      logs: "",
+    };
   },
   created() {
-    getPodContainers("default","ngx1-7c94c8775c-49859").then(rsp=>{
-      this.containers=rsp.data
-    })
+    getPodContainers("default", "ngx1-7c94c8775c-49859").then(rsp => {
+      this.containers = rsp.data;
+    });
   },
   methods: {
-    containerChange(){
-      getPodLogs("default","ngx1-7c94c8775c-49859",this.selectContainer).then(rsp=>{
-        this.logs=rsp.data
-      })
-    }
-  }
-}
-
+    containerChange() {
+      const ns = "default";
+      const podname = "ngx1-7c94c8775c-49859";
+      const cname = this.selectContainer;
+      this.fetchLogs(ns, podname, cname);
+    },
+    fetchLogs(ns, podname, cname) {
+      const url = `http://127.0.0.1:8888/pods/logs?ns=${ns}&podname=${podname}&cname=${cname}`;
+      fetch(url, {
+        method: "GET",
+      }).then(response => {
+        if (response.body) {
+          const reader = response.body.getReader();
+          reader.read().then(function processText({ done, value }) {
+            if (done) {
+              console.log("Stream finished");
+              return;
+            }
+            const chunk = new TextDecoder("utf-8").decode(value, { stream: true });
+            this.logs += chunk;
+            reader.read().then(processText.bind(this));
+          }.bind(this));
+        }
+      });},
+  },
+};
 </script>
 <style>
 .logs {
-  background-color: #8ad9da;
+  max-height: 600px;
+  overflow: auto;
   padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  font-family: 'Courier New', Courier, monospace;
-  font-size: 18px;
-  color: #fff;
-  max-height: 400px;
-  overflow-y: auto;
   margin-top: 20px;
+  background-color: #53c2c5;
+  color: white;
+  border: 1px solid #ccc;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  word-break: break-all;
 }
 
 .logs pre {
   margin: 0;
   padding: 0;
-  white-space: pre-wrap;
-  word-wrap: break-word;
 }
 </style>
